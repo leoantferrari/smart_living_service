07b1b9c68e168c9dff9ecebbc36fb5e0e019d0a5
Leo Ferrari <leoantferrari@gmail.com>
2024-02-04 21:38:22 +0100

    New version

--- src/main/java/com/leoantsmith/SmartLivingBackend/SmartLivingApplication.java
+++ src/main/java/com/leoantsmith/SmartLivingBackend/SmartLivingApplication.java
@@ -15,30 +15,8 @@ import static java.util.Arrays.stream;
 @ConfigurationPropertiesScan
 @EnableScheduling
 public class SmartLivingApplication {
-    private final Logger logger = Logger.getLogger(LightService.class.getName());
 
     public static void main(final String[] args)  {
-        dotEnvSafeCheck();
         SpringApplication.run(SmartLivingApplication.class, args);
-    }
-
-    private static void dotEnvSafeCheck() {
-        final var dotenv = Dotenv.configure()
-                .ignoreIfMissing()
-                .load();
-
-        stream(DotEnv.values())
-                .map(DotEnv::name)
-                .filter(varName -> dotenv.get(varName, "").isEmpty())
-                .findFirst()
-                .ifPresent(varName -> {
-                    throw new RuntimeException("MISSING ENV");
-                });
-    }
-
-    enum DotEnv {
-        THE_DB,
-        DB_USER,
-        DB_PASSWORD
     }
 }
